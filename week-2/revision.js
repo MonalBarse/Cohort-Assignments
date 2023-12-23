fbc35c472d96556fe3cb20b6078500faa9ebcd60
Monal Barse <monal.barse.cer22@itbhu.ac.in>
2023-12-23 20:24:32 +0530

    updated

--- week-2/revision.js
+++ week-2/revision.js
@@ -45,6 +45,7 @@ console.log('Formatted Date with Options:', formattedDateWithOptions);
 
 */
 
+
 // ---------------------------------------- fs (FileSystem) ---------------------------------------- //
 
 /* 
@@ -132,7 +133,6 @@ So, readFile(path[, options], callback) Asynchronously reads the entire contents
 
 */
 
-
 /*
     const promise_2 = new Promise((resolve, reject) => {
         console.log(
@@ -214,3 +214,200 @@ So, readFile(path[, options], callback) Asynchronously reads the entire contents
     
 
  */
+
+// ----------------------------------Difference between Arrow function and normal function---------------------------------- //
+
+/* 
+Syntax:
+
+Normal Function:
+    function add(a, b) {
+        return a + b;
+    }
+
+Arrow Function:
+
+    const add = (a, b) => { 
+        return a + b;
+    }
+
+1. 'this' Binding:
+
+    Normal Function:
+        In normal functions, the value of the 'this' keyword depends on how the function is called. The 'this' value is dynamically determined at runtime.
+
+    Arrow Function:
+        Arrow functions capture the 'this' value from their surrounding lexical context. They don't have their own 'this' binding and inherit it from the enclosing scope.
+
+2. Arguments Object:
+
+    Normal Function:
+        Normal functions have their own arguments object that provides access to all the arguments passed to the function.
+
+    Arrow Function:
+        Arrow functions do not have their own arguments object. Instead, they inherit the arguments object from their enclosing scope.
+
+3. Use of new keyword:
+
+    Normal Function:
+        Normal functions can be used as constructors with the new keyword to create instances of objects.
+
+    Arrow Function:
+        Arrow functions cannot be used as constructors. They do not have their own 'this', and calling them with new will result in an error.
+
+4. prototype Property:
+
+    Normal Function:
+        Normal functions have a prototype property, which can be used for inheritance.
+
+    Arrow Function:
+        Arrow functions do not have a prototype property. They cannot be used as constructors for creating objects with shared properties and methods.
+
+*/
+
+
+//Example of 'this' Binding
+
+    /* 
+    function NormalFunction() {
+    'this'.value = 1;
+
+    setTimeout(function () {
+        'this'.value++;
+        console.log("Inside normal function:", 'this'.value); // 'this' is not the instance of NormalFunction
+    }, 1000);
+    }
+
+    function ArrowFunction() {
+    'this'.value = 1;
+
+    setTimeout(() => {
+        'this'.value++;
+        console.log("Inside arrow function:", 'this'.value); // 'this' is the instance of ArrowFunction
+    }, 1000);
+    }
+
+    const instance1 = new NormalFunction(); // Inside normal function: undefined
+    const instance2 = new ArrowFunction();  // Inside arrow function: 2
+    console.log(instance1.value); // 1
+    console.log(instance1)
+
+    */
+
+// Example of Arguments Object
+
+    /* 
+    //Arrow functions don't have their own arguments object.
+
+    // Normal Function
+    function showArguments() {
+    console.log(arguments); // Normal function has its own arguments object
+    }
+
+    showArguments(1, 2, 3); //[Arguments] { '0': 1, '1': 2, '2': 3 }
+
+    // Arrow Function
+    const showArgumentsArrow = () => {
+    console.log(arguments); // Arrow function doesn't have its own arguments object
+    };
+    */
+
+// Example of Use of new keyword
+
+    /*
+    
+    // Normal Function
+    function Person(name) {
+        this.name = name;
+    }
+  
+    const person1 = new Person("Alice");
+    console.log(person1.name); // Alice
+  
+    // Arrow Function (will throw an error if used with new)
+    const ArrowPerson = (name) => {
+        this.name = name;
+    };
+  
+    // const arrowPerson1 = new ArrowPerson("Bob"); // Error: ArrowPerson is not a constructor
+    // console.log(arrowPerson1.name); // undefined  
+
+    */
+
+// Example of prototype Property
+
+    /* 
+    // Normal Function
+    function Animal(name) {
+        this.name = name;
+    }
+    
+    Animal.prototype.sayName = function () {
+        console.log("Normal Function:", this.name);
+    };
+    
+    const dog = new Animal("Buddy");
+    dog.sayName(); // Normal Function: Buddy
+    
+    // Arrow Function (no prototype property)
+    const ArrowAnimal = (name) => {
+        this.name = name;
+    };
+    
+    // ArrowAnimal.prototype.sayName = function () {}; // TypeError: Cannot set property 'sayName' of undefined
+    // const cat = new ArrowAnimal("Misty"); // TypeError: ArrowAnimal is not a constructor
+  
+    */
+
+// ----------------------------------------Map Filter Reduce--------------------------------------- //
+// ----------------filter--------------------//
+
+/*     
+    
+    let arr =[0,1,2,3,4,5];
+    let my = arr.filter((n)=> {
+        return n;
+    })
+
+    console.log(my);
+
+*/
+
+// ------------------Map----------------- //
+
+// The map() method creates a new array by applying a provided function to every element in the calling array. 
+// It doesn't mutate the original array.
+/*
+    let arr = [0, 1, 2, 3, 4, 5];
+    let myarr = arr.map((n) => {
+        return n + 3;
+    })
+    console.log(myarr); //
+
+*/
+
+// --------------Chaining of Map and filter-----------//
+
+/* 
+    
+    let arr = [0, 1, 2, 3, 4, 5];
+    let myarr = arr 
+    .map((n) => n * 5)
+    .filter((m) => m >= 5);         //Here, 'n' is passed in 'm'
+    console.log(myarr); 
+
+*/
+
+// -----------------Array reduce method---------------//
+
+/*      
+
+        let arr = [0, 1, 2, 3];
+        let myArray = arr.reduce((accumualator, currentValue) => {
+            console.log("accumalator: " + accumualator + ", currentValue: " + currentValue);
+            return accumualator + currentValue
+        }, 5);
+        console.log("myArray = "+ myArray); 
+
+*/   
+//------------------------------------------------//
